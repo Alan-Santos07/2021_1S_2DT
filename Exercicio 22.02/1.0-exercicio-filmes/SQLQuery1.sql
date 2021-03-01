78446c541f75cd5e7dd44c47d359c8a9653f4a7c
Alan da Costa Santos <alancostasantos07@gmail.com>
2021-03-01 10:59:09 -0300

    first commit

---
+++ Exercicio 22.02/1.0-exercicio-filmes/SQLQuery1.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Filmes;
+
+USE Filmes;
+
+CREATE TABLE Generos
+(
+	idGenero INT PRIMARY KEY IDENTITY ,
+	Nome VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE Filmes
+(
+	idFilme INT PRIMARY KEY IDENTITY,
+	idGenero INT FOREIGN KEY REFERENCES Generos(idGenero),
+	Titulo VARCHAR(250) NOT NULL
+);
