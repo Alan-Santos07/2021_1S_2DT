50c3ec5e6c3bdc5fd5721688bed7fed56ddf78b2
Alan-Santos07 <alancostasantos07@gmail.com>
2021-05-20 12:46:30 -0300

    qualquer coisa

--- sprint-4-front-end/react/primeiro-projeto/src/App.js
+++ sprint-4-front-end/react/primeiro-projeto/src/App.js
@@ -1,22 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
+function Hora(props)
+{
+  return <h2>Horário Atual : {props.date.toLocaleTimeString()}</h2>
+} 
+
+class Relogio extends React.Component
+{
+  constructor(props)
+  {
+    super(props)
+    this.state =
+    {
+      date : new Date()
+    }
+  }
+
+  render()
+  {
+    return(
+    <div>
+      <h1> Hora de Aventura </h1>
+      <Hora date={this.state.date} />
+    </div>
+    ) 
+  }
+
+  componentDidMount()
+  {
+    this.timerID = setInterval( () => 
+    {
+      this.ticTac()
+    }, 1000)
+  }
+
+  componentWillUnmount()
+  {
+    clearInterval(this.timerID)
+  }
+
+  ticTac()
+  {
+    this.setState(
+      {
+        date : new Date()
+      }
+    )
+  }
+
+  pausar()
+  {
+    this.state
+  }
+  
+
+}
+
+function App() 
+{
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Relogio />
+        <input id='btn' type='button' value='Parar o Tempo' onclick={componentWillUnmount()}/>
+        <Relogio />
+        <input id='btn' type='button' value='Parar o Tempo' onclick={componentWillUnmount()}/>
       </header>
     </div>
   );
