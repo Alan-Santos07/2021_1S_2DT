e0367e32c196f6db2472806923fbb7c31b6ef978
Alan Santos <alan.santos@portalsesisp.org.br>
2021-07-12 15:39:01 -0300

    Mobile funcional

--- sp-medical-group/back/Sp_medical_group_tarde/Sp_medical_group_tarde/Context/MedicalContext.cs
+++ sp-medical-group/back/Sp_medical_group_tarde/Sp_medical_group_tarde/Context/MedicalContext.cs
@@ -32,7 +32,7 @@ namespace Sp_medical_group_tarde.Context
             if (!optionsBuilder.IsConfigured)
             {
 // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-NQMUPH6\\SQLEXPRESS; Initial Catalog=Medical; user id=sa; pwd=senai@132;");
+                optionsBuilder.UseSqlServer("Data Source=LAB08DESK501\\SQLEXPRESS; Initial Catalog=Medical; user id=sa; pwd=senai@132;");
             }
         }
 
